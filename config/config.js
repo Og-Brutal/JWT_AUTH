099fe365ee843fca72d55d53ceaf857bf927a6e9
Og-Brutal <ogbrutal2825@gmail.com>
2026-03-17 15:59:25 +0500

    Remove .env from repository

--- config/config.js
+++ config/config.js
@@ -8,8 +8,25 @@ if (!process.env.MONGO_URI) {
 if (!process.env.JWT_SCRET_KEY) {
     throw new Error("JWT_SCRET_KEY is missing in .env");
 }
+if (!process.env.GOOGLE_CLIENT_ID) {
+    throw new Error("GOOGLE_CLIENT_ID is missing in .env");
+}
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+    throw new Error("GOOGLE_CLIENT_SECRET is missing in .env");
+}
+if (!process.env.GOOGLE_REFRESH_TOKEN) {
+    throw new Error("GOOGLE_REFRESH_TOKEN is missing in .env");
+}
+if (!process.env.GOOGLE_USER) {
+    throw new Error("GOOGLE_USER is missing in .env");
+}
+
 
 export default {
     MONGO_URI: process.env.MONGO_URI,
-    JWT_SCRET_KEY:process.env.JWT_SCRET_KEY
+    JWT_SCRET_KEY:process.env.JWT_SCRET_KEY,
+    GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_REFRESH_TOKEN:process.env.GOOGLE_REFRESH_TOKEN,
+    GOOGLE_USER:process.env.GOOGLE_USER
 };
